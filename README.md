4af65c3ac6feac1a427849ef6d5e5f02086dd50e
thisisaaroland <aaron@mapzen.com>
2015-07-01 15:36:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# www-nearby-map
+
+A simple web application that used the [Mapzen Vector Tile
+Service](https://mapzen.com/projects/vector-tiles) to tell you what features in
+OpenStreetMap are nearby.
+
+## Usage
+
+1) You will need an API key to use the Mapzen Vector Tile Service. They are free
+and easy to get by visiting: https://mapzen.com/developers
+
+2) Once you've created an API key you will need to make a copy of the
+`www/config.js.example` named... you got it: `www/config.js`. Once you have look
+inside the file and replace the placeholder API key with your own.
+
+3) Open `www/index.html` in your web browser. Walk around. Repeat.
---
+++ www/index.js
@@ -0,0 +1,80 @@
+var MM = com.modestmaps;
+var mm = new MM.TemplatedMapProvider('https://vector.mapzen.com/osm/all/{Z}/{X}/{Y}.json?api_key=' + mapzen_apikey);	// see also: config.js
+
+var last_url = undefined;	// please don't make me a global... maybe... does it really matter?
+
+function init(){
+
+	if (! "geolocation" in navigator){
+		return;
+	}
+
+	var geo_options = {
+		enableHighAccuracy: true, 
+		maximumAge        : 30000, 
+		timeout           : 27000
+	};
+
+	var wpid = navigator.geolocation.watchPosition(on_location, on_not_location, geo_options);
+}
+
+function on_location(position){
+
+	console.log(position.coords.latitude, position.coords.longitude);
+
+	var loc = new MM.Location(position.coords.latitude, position.coords.longitude);
+
+	var coord = mm.locationCoordinate(loc);
+	coord = coord.zoomTo(18);
+	
+	var url = mm.getTileUrl(coord);
+	fetch_data(url);
+}
+
+function on_not_location(){
+	console.log("SAD");
+}
+
+function fetch_data(url){
+
+	if (url == last_url){
+		console.log("I haven't moved!");
+		return;
+	}
+
+	var req = new XMLHttpRequest();
+	req.onload = on_data;
+	req.open("get", url, true);
+	req.send();
+}
+
+function on_data(){
+
+	last_url = this.responseURL;
+
+	data = JSON.parse(this.responseText);
+
+	var html = "";
+	html += "<ul>";
+	
+	for (k in data){
+		
+		var features = data[k]['features'];
+		var count = features.length;
+		
+		if (count > 1){
+			html += "<li>" + count + " " + k + "</li>";
+		}
+		
+		else if (count == 1){
+			html += "<li>one " + k + "</li>";
+		}
+		
+		else {
+			html += "<li>no " + k + "</li>";
+		}
+	}
+	
+	var n = document.getElementById("nearby");
+	n.innerHTML = html;
+}
